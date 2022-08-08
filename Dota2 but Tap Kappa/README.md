af7e9c50e4ddf05b6709b89e559a799b45370a02
Zeno-PT <peeranath2544@gmail.com>
2022-08-08 20:24:34 +0700

    sixth commit

--- Dota2 but Tap Kappa/README.md
+++
@@ -1,2 +0,0 @@
-# Tap_Kappa
-Application Game ที่นำไอเดียมาจากเกม Tap Titan นำมาผสมผสานตัวละครและสกิลจากเกม Dota2 
--- Dota2 but Tap Kappa/src/logic/game/Attackable.java
+++
@@ -1,5 +0,0 @@
-package logic.game;
-
-public interface Attackable {
-	public abstract void attack() throws InsertAnimationPictureFailedException;
-}
